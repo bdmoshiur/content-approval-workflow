4e017a7b922d5ae031bdb58bb09ac6f1be13aa51
moshiur rahman <moshiurcse888@gmail.com>
2024-12-09 22:58:51 +0600

    update

--- src/Providers/ContentApprovalWorkflowServiceProvider.php
+++ src/Providers/ContentApprovalWorkflowServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\ContentApprovalWorkflow\Providers;
+namespace Interlink\ContentApprovalWorkflow\Providers; // Add the correct namespace here
 
 use Illuminate\Support\ServiceProvider;
 
@@ -8,16 +8,24 @@ class ContentApprovalWorkflowServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        // Merge configuration file from the config directory
         $this->mergeConfigFrom(__DIR__ . '/../Config/config.php', 'content-approval-workflow');
     }
 
     public function boot()
     {
+        // Load routes from the Routes directory
         $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
+
+        // Load migrations from the database/migrations directory
         $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+
+        // Publish configuration file to the Laravel config path
         $this->publishes([
             __DIR__ . '/../Config/config.php' => config_path('content-approval-workflow.php'),
         ], 'config');
+
+        // Publish views to the Laravel views path
         $this->publishes([
             __DIR__ . '/../Views' => resource_path('views/vendor/content-approval-workflow'),
         ], 'views');
